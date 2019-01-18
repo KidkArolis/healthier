6e4d579fcf973b6a6503d8bfb0cee5885e020c1b
Karolis Narkevicius <karolis.n@gmail.com>
2019-01-18 23:09:33 +0000

    Reformat the code using itself, add real tests

--- test/api.js
+++ test/api.js
@@ -1,17 +1,60 @@
-var path = require('path')
-var prettyStandard = require('../')
-var test = require('tape')
-var filePath = path.resolve('./bin/cmd.js')
+// var path = require("path");
+var prettyStandard = require("../");
+var test = require("tape");
+// var filePath = path.resolve("./bin/cmd.js");
 
-test('api usage', function (t) {
-  t.plan(6)
-  prettyStandard.lintFiles([], { cwd: 'bin' }, function (err, result) {
-    t.error(err, 'no error while linting')
-    t.equal(typeof result, 'object', 'result is an object')
-    t.equal(result.errorCount, 2, 'error count 2')
+test("api usage", function(t) {
+  t.plan(3);
+  prettyStandard.lintFiles([], { cwd: "bin" }, function(err, result) {
+    t.error(err, "no error while linting");
+    t.equal(typeof result, "object", "result is an object");
+    t.equal(result.errorCount, 0, "no errors");
 
-    t.equal(path.resolve(result.results[0].filePath), filePath, 'error filepath correct')
-    t.equal(result.results[0].messages[0].message, 'Missing semicolon.', 'first mising semicolon message')
-    t.equal(result.results[0].messages[0].message, 'Missing semicolon.', 'second mising semicolon message')
-  })
-})
+    // t.equal(
+    //   path.resolve(result.results[0].filePath),
+    //   filePath,
+    //   "error filepath correct"
+    // );
+    // t.equal(
+    //   result.results[0].messages[0].message,
+    //   "Missing semicolon.",
+    //   "first mising semicolon message"
+    // );
+    // t.equal(
+    //   result.results[0].messages[0].message,
+    //   "Missing semicolon.",
+    //   "second mising semicolon message"
+    // );
+  });
+});
+
+test("prettier style", function(t) {
+  t.plan(4);
+  const result = prettyStandard.lintTextSync(`const a = '2'`);
+  t.equal(typeof result, "object", "result is an object");
+  t.equal(result.errorCount, 2, "2 errors");
+
+  t.equal(
+    result.results[0].messages[0].message,
+    "'a' is assigned a value but never used.",
+    "first message"
+  );
+  t.equal(
+    result.results[0].messages[1].message,
+    "Replace `'2'` with `\"2\";⏎`",
+    "second message"
+  );
+});
+
+test("standard code quality", function(t) {
+  t.plan(3);
+  const result = prettyStandard.lintTextSync(`a();\n`);
+  t.equal(typeof result, "object", "result is an object");
+  t.equal(result.errorCount, 1, "1 errors");
+
+  t.equal(
+    result.results[0].messages[0].message,
+    "'a' is not defined.",
+    "first message"
+  );
+});
